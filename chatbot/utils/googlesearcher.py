7a4742b49bf51beea4f422f23801300292062952
sparkai-ca <sparkai.ca@gmail.com>
2021-01-19 19:16:27 +0500

    v4

--- chatbot/utils/googlesearcher.py
+++ chatbot/utils/googlesearcher.py
@@ -7,16 +7,16 @@ class BtsScraper:
     def __init__(self):
         options = webdriver.ChromeOptions()
 
-        # options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
-        # options.add_argument("--headless")
-        # options.add_argument("--disable-dev-shm-usage")
-        # options.add_argument("--no-sandbox")
-        # self.driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=options)
-
+        options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
         options.add_argument("--headless")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--no-sandbox")
-        self.driver = webdriver.Chrome(executable_path='static/chatbot_staticfiles/chromedriver', chrome_options=options)
+        self.driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=options)
+
+        # options.add_argument("--headless")
+        # options.add_argument("--disable-dev-shm-usage")
+        # options.add_argument("--no-sandbox")
+        # self.driver = webdriver.Chrome(executable_path='static/chatbot_staticfiles/chromedriver', chrome_options=options)
 
 
     def searchQuery(self, query):
